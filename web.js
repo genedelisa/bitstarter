8545bd3183a2669259ec2b344a0d69029e66992d
Gene De Lisa <gene@rockhoppertech.com>
2013-07-08 20:40:23 +0000

    reading fs

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fs = require('fs');
+
+var buffer;
+
+fs.readFileSync('index.html', function(err, data) {
+    if(err) {
+	throw err;
+    }
+    buffer = new Buffer(data, 'utf-8');
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
